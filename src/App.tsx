699c005616d908023b5667ed7d4e131ea0e1c400
Estelle Pieragnolo <estellepieragnolo@gmail.com>
2022-10-27 10:16:05 +0100

    api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const getData = async () => {
+      const baseUrl = 'https://books.googleapis.com/books/v1/volumes?q=gardening&maxResults=20&orderBy=newest&key=AIzaSyCCFMYb0rvuibTlkknulNp5nQygOs4JPi4'; // Put apiKey elsewhere
+      try {
+        const response = await fetch(`${baseUrl}`, {
+          method: 'GET'
+        });
+        try {
+          const data = await response.json();
+          if (!response.ok) {
+            const error = response.status;
+            throw new Error(error.toString());
+          }
+          setData(data.items.map(item => item.volumeInfo))
+        }
+        catch (error) {
+          console.log(`error parsing data ${error}`)
+        }
+      }
+      catch (error) {
+        console.log(`error fetching data ${error}`)
+      }
+
+    }
+    getData();
+  }, [])
+  console.log({ data })
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="App" >
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
