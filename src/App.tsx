3ffc8b88f2a1a36caaabd01d1cdbbd47776ab6a0
Estelle Pieragnolo <estellepieragnolo@gmail.com>
2022-10-28 08:45:37 +0100

    create home page with book selection with types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
+import { TVolumeInfo } from './types';
 
 const App = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<TVolumeInfo[]>([]);
+  const apiKey = import.meta.env.VITE_API_KEY
 
   useEffect(() => {
     const getData = async () => {
-      const baseUrl = 'https://books.googleapis.com/books/v1/volumes?q=gardening&maxResults=20&orderBy=newest&key=AIzaSyCCFMYb0rvuibTlkknulNp5nQygOs4JPi4'; // Put apiKey elsewhere
+      const baseUrl = `https://books.googleapis.com/books/v1/volumes?q=gardening&maxResults=20&orderBy=newest&key=${apiKey}` // Put apiKey elsewhere
       try {
         const response = await fetch(`${baseUrl}`, {
           method: 'GET'
@@ -16,7 +18,23 @@ const App = () => {
             const error = response.status;
             throw new Error(error.toString());
           }
-          setData(data.items.map(item => item.volumeInfo))
+          setData(data.items.map((item: { volumeInfo: TVolumeInfo }) => {
+            console.log(item.volumeInfo.authors)
+            const info = item.volumeInfo
+            return ({
+              authors: info.authors,
+              categories: info.categories,
+              description: info.description,
+              imageLinks: info.imageLinks,
+              industryIdentifiers: info.industryIdentifiers,
+              language: info.language,
+              pageCount: info.pageCount,
+              previewLink: info.previewLink,
+              publishedDate: info.publishedDate,
+              publisher: info.publisher,
+              title: info.title,
+            })
+          }))
         }
         catch (error) {
           console.log(`error parsing data ${error}`)
@@ -32,6 +50,20 @@ const App = () => {
   console.log({ data })
   return (
     <div className="App" >
+      <h1>Welcome to Colibri Project</h1>
+      <h3>Our book selection</h3>
+      <div>
+        {data?.map((bookInfo, i) => {
+          return (
+            <div key={`book-${i}`}>
+              <a href={bookInfo.previewLink} target="_blank">
+                <h4>{bookInfo.title}</h4>
+                <p>{bookInfo?.authors}</p>
+                <img src={bookInfo.imageLinks.thumbnail} alt="book_thumbnail" />
+              </a>
+            </div>)
+        })}
+      </div>
     </div>
   );
 }
