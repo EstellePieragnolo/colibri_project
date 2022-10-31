5b547e1ba217c57841c459ef90757bb3f7ca0489
Estelle Pieragnolo <estellepieragnolo@gmail.com>
2022-10-31 11:50:53 +0000

    data from homeData() + display category title

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,16 @@
-import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import BookSelection from './BookSelection';
+import homeData from './data/homeData';
 import { TVolumeInfo } from './types';
 
 const App = () => {
-  const [data, setData] = useState<TVolumeInfo[]>([]);
-  const apiKey = import.meta.env.VITE_API_KEY
-
-  useEffect(() => {
-    const getData = async () => {
-      const baseUrl = `https://books.googleapis.com/books/v1/volumes?q=gardening&maxResults=20&orderBy=newest&key=${apiKey}` // Put apiKey elsewhere
-      try {
-        const response = await fetch(`${baseUrl}`, {
-          method: 'GET'
-        });
-        try {
-          const data = await response.json();
-          if (!response.ok) {
-            const error = response.status;
-            throw new Error(error.toString());
-          }
-          setData(data.items.map((item: { volumeInfo: TVolumeInfo }) => {
-            console.log(item.volumeInfo.authors)
-            const info = item.volumeInfo
-            return ({
-              authors: info.authors,
-              categories: info.categories,
-              description: info.description,
-              imageLinks: info.imageLinks,
-              industryIdentifiers: info.industryIdentifiers,
-              language: info.language,
-              pageCount: info.pageCount,
-              previewLink: info.previewLink,
-              publishedDate: info.publishedDate,
-              publisher: info.publisher,
-              title: info.title,
-            })
-          }))
-        }
-        catch (error) {
-          console.log(`error parsing data ${error}`)
-        }
-      }
-      catch (error) {
-        console.log(`error fetching data ${error}`)
-      }
-
-    }
-    getData();
-  }, [])
-  console.log({ data })
+  const category = 'middle-ages'
+  const data = homeData(category)
   return (
     <Section className="App" >
       <AppTitle>Welcome to Colibri Project</AppTitle>
       <AppSubtitle>Our book selection</AppSubtitle>
+      <CategoryTitle>{category}</CategoryTitle>
       <BooksContainer>
         {data?.map((bookInfo: TVolumeInfo, i: Number) => {
           return (
@@ -76,8 +33,9 @@ const BooksContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 40px;
-  margin: 150px auto 0;
+  margin: 0 auto 0;
   max-width: 1000px;
+  padding-top:70px;
 `
 const AppTitle = styled.h1`
   margin-bottom: 40px;
@@ -85,4 +43,13 @@ const AppTitle = styled.h1`
 const AppSubtitle = styled.h3`
   color: #074b86;
   font-size: 25px;
+`
+const CategoryTitle = styled.h4`
+  text-transform: capitalize;
+  margin-top: 70px;
+  font-size: 18px;
+  border: 1px solid #213547;
+  padding: 25px 0;
+  max-width: 1000px;
+  margin: 70px auto 0;
 `
