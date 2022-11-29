242867e7148cee2d364f0dcaabf115fc21bc5a5b
Estelle Pieragnolo <estellepieragnolo@gmail.com>
2022-11-29 09:46:37 +0000

    test remove from wishlist

--- src/components/Wishlist/WishlistSlice.test.ts
+++ src/components/Wishlist/WishlistSlice.test.ts
@@ -1,8 +1,8 @@
-import { addToWishlist } from "./WishlistSlice";
+import { addToWishlist,removeFromWishlist } from "./WishlistSlice";
 import { expect, it } from "vitest";
 import {store} from "../../redux/store";
 
-it("Adds a new book", () => {
+it("Add a book in wishlist", () => {
   let state = store.getState().wishlist;
   const initialWishlist = state.wishlist.length;
   store.dispatch(addToWishlist("1"));
@@ -10,4 +10,13 @@ it("Adds a new book", () => {
   const newlyAddedBook = state.wishlist.find((wishlistId) => { return wishlistId.id === "1"});
   expect(newlyAddedBook?.id).toBe("1");
   expect(state.wishlist.length).toBeGreaterThan(initialWishlist);
+});
+
+
+it("Remove a book from wishlist", () => {
+  let state = store.getState().wishlist;
+  const initialWishlist = state.wishlist.length;
+  store.dispatch(removeFromWishlist("1"));
+  state = store.getState().wishlist;
+  expect(state.wishlist.length).toBeLessThan(initialWishlist);
 });
